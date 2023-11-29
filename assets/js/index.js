7ca87455f052832e9cecc112aa88600e28cfba69
digitalflowerstudio <floriantappeser@gmail.com>
2023-11-29 12:51:44 +0100

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,62 @@
-const lenis = new Lenis()
+// Cheat Sheet Hover Modals
 
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+var n = 0;
+$(".content-item")
+  .mouseenter(function () {
+    $(this)
+      .children(".content-item-desc")
+      .toggleClass("content-item-desc_hide");
+  })
+  .mouseleave(function () {
+    $(this)
+      .children(".content-item-desc")
+      .toggleClass("content-item-desc_hide");
+  });
+
+$(".replaceMeTo").hover(
+  function () {
+    $(this).text("100%");
+  },
+  function () {
+    $(this).text("to");
+  }
+);
+
+$(".replaceMeFrom").hover(
+  function () {
+    $(this).text("0%");
+  },
+  function () {
+    $(this).text("from");
+  }
+);
+
+// Smooth Scrolling
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
-
+requestAnimationFrame(raf);
 
 // Button
-
 
 // JavaScript used to set randomness for particles.
 // Could be done via SSR
 
 const RANDOM = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-const PARTICLES = document.querySelectorAll('.particle');
-PARTICLES.forEach(P => {
-  P.setAttribute('style', `
+const PARTICLES = document.querySelectorAll(".particle");
+PARTICLES.forEach((P) => {
+  P.setAttribute(
+    "style",
+    `
 		--x: ${RANDOM(20, 80)};
 		--y: ${RANDOM(20, 80)};
 		--duration: ${RANDOM(6, 20)};
@@ -30,35 +65,7 @@ PARTICLES.forEach(P => {
 		--origin-x: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
 		--origin-y: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
 		--size: ${RANDOM(40, 90) / 100};
-	`);
+	`
+  );
 });
 //# sourceURL=pen.js
-
-// Cheat Sheet Hover Modals
-
-var n = 0;
-$(".content-item")
-	.mouseenter(function () {
-		$(this).children(".content-item-desc").toggleClass("content-item-desc_hide");
-	})
-	.mouseleave(function () {
-		$(this).children(".content-item-desc").toggleClass("content-item-desc_hide");
-	});
-
-$(".replaceMeTo").hover(
-	function () {
-		$(this).text("100%");
-	},
-	function () {
-		$(this).text("to");
-	}
-);
-
-$(".replaceMeFrom").hover(
-	function () {
-		$(this).text("0%");
-	},
-	function () {
-		$(this).text("from");
-	}
-);
